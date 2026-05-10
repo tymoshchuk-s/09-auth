18fab3935f2bbc69db223543849354aab8842d72
Paris <serhii.tymoshchuk@icloud.com>
2026-05-11 01:07:12 +0200

    update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -6,7 +6,7 @@ import { checkServerSession } from './lib/api/serverApi';
 const privateRoutes = ['/profile', '/notes'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const cookieStore = await cookies();
